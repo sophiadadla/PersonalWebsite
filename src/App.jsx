ead1dd346c3be5af44088328bb0b413fd84bc3fd
sophiadadla <dadlas@ufl.edu>
2025-06-22 17:29:44 -0400

    fixing router issues

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <HashRouter basename="/PersonalWebsite">
+      <HashRouter basename="https://sophiadadla.github.io/PersonalWebsite/">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
